8636ed1fd25278236206d7a9588bf1716d209408
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-12-08 03:13:18 -0600

    add a space

--- lib/Net/LDAP/posixGroup.pm
+++ lib/Net/LDAP/posixGroup.pm
@@ -23,7 +23,7 @@ our $VERSION = '1.0.0';
 
     my $foo = Net::LDAP::posixGroup->new(baseDN=>'ou=group,dc=foo');
 
-    #creates a new for the group newGroup with a GID of 404 and members of user1 and user2.
+    # creates a new for the group newGroup with a GID of 404 and members of user1 and user2.
     my $entry = $foo->create(name=>'newGroup', gid=>'404', members=>['user1', 'user2']);
 
     print $entry->ldif;
